4aaf6cc8526c465de0f04bd5e9a5167281962d8e
austinc512 <85425656+austinc512@users.noreply.github.com>
2023-07-23 14:09:12 -0500

    starting win logic

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 Instructions:
 
-1. Sign up for api key
+1. Sign up for api key (this no longer a requirement as of Summer 2023)
 2. Create an arena that displays two Pokemon battling each other.
 3. Compare the two to see who is likely to win.
--- main.js
+++ main.js
@@ -8,6 +8,8 @@ class Pokemon {
       "submit",
       this.handleFormSubmit.bind(this)
     );
+    this.name = "";
+    this.types = [];
   }
   handleFormSubmit = (event) => {
     event.preventDefault();
@@ -19,6 +21,16 @@ class Pokemon {
       .then((response) => response.json())
       .then((response) => {
         console.log(response);
+        return response;
+      })
+      .then((response) => {
+        // capturing name and types from API response
+        this.name = response.name;
+        console.log(this.name);
+        for (let item of response.types) {
+          this.types.push(item.type.name);
+        }
+        console.log(this.types);
         return response;
       })
       .then((response) => {
@@ -64,4 +76,126 @@ Other tasks:
 -make UI look nice
 -make win logic
 
+as a first-level property from the GET charizard response:
+
+  "types": [
+    {
+      "slot": 1,
+      "type": {
+        "name": "fire",
+        "url": "https://pokeapi.co/api/v2/type/10/"
+      }
+    },
+    {
+      "slot": 2,
+      "type": {
+        "name": "flying",
+        "url": "https://pokeapi.co/api/v2/type/3/"
+      }
+    }
+  ],
+
+
+const friendTypes = [];  
+for (let item of response.types) {
+  friendTypes.push(item.type.name)
+}  
+
+
+
+GET https://pokeapi.co/api/v2/type/fire/
+
+
+{
+    "damage_relations": {
+        "double_damage_from": [
+            {
+                "name": "ground",
+                "url": "https://pokeapi.co/api/v2/type/5/"
+            },
+            {
+                "name": "rock",
+                "url": "https://pokeapi.co/api/v2/type/6/"
+            },
+            {
+                "name": "water",
+                "url": "https://pokeapi.co/api/v2/type/11/"
+            }
+        ],
+        "double_damage_to": [
+            {
+                "name": "bug",
+                "url": "https://pokeapi.co/api/v2/type/7/"
+            },
+            {
+                "name": "steel",
+                "url": "https://pokeapi.co/api/v2/type/9/"
+            },
+            {
+                "name": "grass",
+                "url": "https://pokeapi.co/api/v2/type/12/"
+            },
+            {
+                "name": "ice",
+                "url": "https://pokeapi.co/api/v2/type/15/"
+            }
+        ],
+        "half_damage_from": [
+            {
+                "name": "bug",
+                "url": "https://pokeapi.co/api/v2/type/7/"
+            },
+            {
+                "name": "steel",
+                "url": "https://pokeapi.co/api/v2/type/9/"
+            },
+            {
+                "name": "fire",
+                "url": "https://pokeapi.co/api/v2/type/10/"
+            },
+            {
+                "name": "grass",
+                "url": "https://pokeapi.co/api/v2/type/12/"
+            },
+            {
+                "name": "ice",
+                "url": "https://pokeapi.co/api/v2/type/15/"
+            },
+            {
+                "name": "fairy",
+                "url": "https://pokeapi.co/api/v2/type/18/"
+            }
+        ],
+        "half_damage_to": [
+            {
+                "name": "rock",
+                "url": "https://pokeapi.co/api/v2/type/6/"
+            },
+            {
+                "name": "fire",
+                "url": "https://pokeapi.co/api/v2/type/10/"
+            },
+            {
+                "name": "water",
+                "url": "https://pokeapi.co/api/v2/type/11/"
+            },
+            {
+                "name": "dragon",
+                "url": "https://pokeapi.co/api/v2/type/16/"
+            }
+        ],
+        "no_damage_from": [],
+        "no_damage_to": []
+    },
+
+...
+}
+
 */
+
+const compareBtn = document.getElementById("comparePokemon");
+compareBtn.addEventListener("click", function () {
+  // do something
+  console.log(friendPokemon.types);
+  console.log(enemyPokemon.types);
+});
