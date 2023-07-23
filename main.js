2046190bc627825ee29e54b008fb9008d5216064
austinc512 <85425656+austinc512@users.noreply.github.com>
2023-07-22 22:28:23 -0500

    added Pokemon class

--- main.js
+++ main.js
@@ -1,67 +1,122 @@
-// function getPokemon(str) {
-//   const query = str.toLowerCase().trim();
-//   fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+// const myPokemon = document.getElementById("myPokemon");
+// myPokemon.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   const pokemonName = document.getElementById("chooseMyPokemon").value;
+//   console.log(pokemonName);
+//   const cleanName = pokemonName.toLowerCase().trim();
+//   console.log(cleanName);
+//   fetch(`https://pokeapi.co/api/v2/pokemon/${cleanName}`)
 //     .then((response) => response.json())
 //     .then((response) => {
 //       console.log(response);
 //       return response;
 //     })
-//     .then((response) => (Charizard = response));
-// }
+//     .then((response) => {
+//       console.log(response.sprites.back_default);
+//       let html = `
+//       <div class="data-unit">
+//         <img src="${response.sprites.back_default}">
+//       </div>`;
+//       document
+//         .getElementById("displayMyPokemon")
+//         .insertAdjacentHTML("beforeend", html);
+//     });
+// });
 
-let storeMyPokemon;
-
-const myPokemon = document.getElementById("myPokemon");
-myPokemon.addEventListener("submit", function (e) {
-  e.preventDefault();
-  // e.stopPropagation();
-  const pokemonName = document.getElementById("chooseMyPokemon").value;
-  console.log(pokemonName);
-  const cleanName = pokemonName.toLowerCase().trim();
-  console.log(cleanName);
-  fetch(`https://pokeapi.co/api/v2/pokemon/${cleanName}`)
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response);
-      return response;
-    })
-    .then((response) => {
-      console.log(response.sprites.back_default);
-      let html = `
+class Pokemon {
+  constructor(textInput, displayArea, formElement, friend) {
+    this.textInput = textInput;
+    this.displayArea = displayArea;
+    this.formElement = formElement;
+    this.friend = friend;
+    this.formElement.addEventListener(
+      "submit",
+      this.handleFormSubmit.bind(this)
+    );
+  }
+  handleFormSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    // Your form submission handling code here
+    // For example, you can access form inputs, validate data, or send data to the server
+    event.stopPropagation();
+    // console.log("Form submitted!");
+    console.log(this.textInput);
+    const cleanName = this.textInput.value.toLowerCase().trim();
+    fetch(`https://pokeapi.co/api/v2/pokemon/${cleanName}`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        return response;
+      })
+      .then((response) => {
+        console.log(
+          this.friend == "friend"
+            ? response.sprites.back_default
+            : response.sprites.front_default
+        );
+        const friendOrFoe =
+          this.friend == "friend"
+            ? response.sprites.back_default
+            : response.sprites.front_default;
+        // response.sprites.front_default
+        let html = `
       <div class="data-unit">
-        <img src="${response.sprites.back_default}">
+        <img src="${friendOrFoe}">
       </div>`;
-      document
-        .getElementById("displayMyPokemon")
-        .insertAdjacentHTML("beforeend", html);
-    });
-});
+        this.displayArea.insertAdjacentHTML("beforeend", html);
+      });
 
-const myPokemon2 = document.getElementById("myPokemon2");
-myPokemon2.addEventListener("submit", function (e) {
-  e.preventDefault();
-  // e.stopPropagation();
-  const pokemonName = document.getElementById("chooseMyPokemon2").value;
-  console.log(pokemonName);
-  const cleanName = pokemonName.toLowerCase().trim();
-  console.log(cleanName);
-  fetch(`https://pokeapi.co/api/v2/pokemon/${cleanName}`)
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response);
-      return response;
-    })
-    .then((response) => {
-      console.log(response.sprites.front_default);
-      let html = `
-      <div class="data-unit">
-        <img src="${response.sprites.front_default}">
-      </div>`;
-      document
-        .getElementById("displayMyPokemon2")
-        .insertAdjacentHTML("beforeend", html);
-    });
-});
+    // If you want to access form data, you can use the following:
+    // const formData = new FormData(this.formElement); // This will give you a FormData object containing form field data
+  };
+}
+
+/*
+textInput, displayArea, formElement
+document.getElementById('chooseMyPokemon'), document.getElementById('displayMyPokemon'), document.getElementById('myPokemon')
+*/
+
+const friendInput = document.getElementById("chooseMyPokemon");
+const friendDisplay = document.getElementById("displayMyPokemon");
+const friendForm = document.getElementById("myPokemon");
+
+const friendPokemon = new Pokemon(
+  friendInput,
+  friendDisplay,
+  friendForm,
+  "friend"
+);
+
+const enemyInput = document.getElementById("chooseMyPokemon2");
+const enemyDisplay = document.getElementById("displayMyPokemon2");
+const enemyForm = document.getElementById("myPokemon2");
+
+const enemyPokemon = new Pokemon(enemyInput, enemyDisplay, enemyForm, "enemy");
+
+// const myPokemon2 = document.getElementById("myPokemon2");
+// myPokemon2.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   const pokemonName = friendInput.value;
+//   console.log(pokemonName);
+//   const cleanName = pokemonName.toLowerCase().trim();
+//   console.log(cleanName);
+//   fetch(`https://pokeapi.co/api/v2/pokemon/${cleanName}`)
+//     .then((response) => response.json())
+//     .then((response) => {
+//       console.log(response);
+//       return response;
+//     })
+//     .then((response) => {
+//       console.log(response.sprites.front_default);
+//       let html = `
+//       <div class="data-unit">
+//         <img src="${response.sprites.front_default}">
+//       </div>`;
+//       document
+//         .getElementById("displayMyPokemon2")
+//         .insertAdjacentHTML("beforeend", html);
+//     });
+// });
 
 // .then((response) => (Charizard = response));
 // sprites.frontDefault
@@ -87,14 +142,39 @@ What things do we need for a pokemon?
 
 */
 
-class Human {
-  constructor(name, height, weight, dob) {
-    this.name = name;
-    this.height = height;
-    this.weight = weight;
-    this.dob = dob;
+/*
+textInput, displayArea, formElement
+document.getElementById('chooseMyPokemon'), document.getElementById('displayMyPokemon'), document.getElementById('myPokemon')
+*/
+
+/*
+class MyClass {
+  constructor() {
+    this.formElement = document.getElementById("myForm"); // Assuming you have a form element with the ID "myForm"
+
+    // Add the event listener to the form
+    this.formElement.addEventListener(
+      "submit",
+      this.handleFormSubmit.bind(this)
+    );
   }
-  greeting() {
-    return "Hello " + this.name + "!";
+
+  // Define the event handler method for form submission
+  handleFormSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Your form submission handling code here
+    // For example, you can access form inputs, validate data, or send data to the server
+    console.log("Form submitted!");
+
+    // If you want to access form data, you can use the following:
+    // const formData = new FormData(event.target); // This will give you a FormData object containing form field data
   }
+
+  // Other methods and properties of the class can be defined here
 }
+
+// Create an instance of the class
+const myInstance = new MyClass();
+
+*/
